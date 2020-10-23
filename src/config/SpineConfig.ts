da0791ed1f5aa61af50b524d795792bbf6af91b5
genesis-steve <steve.x@gen-game.com>
2020-10-23 18:21:34 +0800

    get animation name through clicking  animation button

--- src/config/SpineConfig.ts
+++ src/config/SpineConfig.ts
@@ -1,14 +1,46 @@
 export class SpineConfig implements ISpineConfig {
 	public assetName: string = 'LightningTallyMeter';
 	public position: IPoint;
+
+	public displayButtonContainer: IDivStyle = {
+		position: 'absolute',
+		overflow: 'auto',
+		x: 730,
+		y: 10,
+		width: 600,
+		height: 1280,
+		fontSize: 30
+	};
+
+	public animationMixer: IDivStyle = {
+		position: 'absolute',
+		overflow: 'auto',
+		x: this.displayButtonContainer.x + this.displayButtonContainer.width,
+		y: 10,
+		width: 900,
+		height: 1280,
+		fontSize: 30
+	};
 }
 
 export interface ISpineConfig {
 	assetName: string;
 	position: IPoint;
+	displayButtonContainer: IDivStyle;
+	animationMixer: IDivStyle;
 }
 
 export interface IPoint {
 	x: number;
 	y: number;
+}
+
+export interface IDivStyle {
+	position: string;
+	overflow?: string;
+	x: number;
+	y: number;
+	width: number;
+	height: number;
+	fontSize: number;
 }
--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,18 @@ export class GmaeApplication {
 	protected pixi: Application;
 	protected animation: spine.Spine;
 
+	protected animationList: string[];
+	protected inputButtons: HTMLElement[];
+
+	protected countMixGroup: number = 0;
+
+	protected animationMixer: HTMLElement;
+
+	protected waitInputData: IWaitInputData = {
+		isWaiting: false,
+		targetId: ''
+	};
+
 	constructor () {
 		this.appConfig = new MainConfig();
 		this.pixi = new Application( this.appConfig );
@@ -38,36 +50,132 @@ export class GmaeApplication {
 				this.animation.renderable = false;
 				this.addChild( this.animation );
 				this.createTestButtons();
+				this.createAnimationMixer();
+				this.addEventListener();
 			} );
 	}
 
 	protected createTestButtons (): void {
-		const buttonContainer = document.getElementById( 'buttonContainer' );
-		buttonContainer.style.position = 'absolute';
-		buttonContainer.style.top = '10px';
-		buttonContainer.style.left = '730px';
-		buttonContainer.style.height = '1280px';
-		buttonContainer.style.overflow = 'auto';
+		const buttonContainer = document.getElementById( 'displayButtonContainer' );
+		const config = this.spineConfig.displayButtonContainer;
+		buttonContainer.style.position = config.position;
+		buttonContainer.style.overflow = config.overflow;
+		buttonContainer.style.top = `${ config.y }px`;
+		buttonContainer.style.left = `${ config.x }px`;
+		buttonContainer.style.width = `${ config.width }px`;
+		buttonContainer.style.height = `${ config.height }px`;
 
-		let animationList: string[] = [];
+		this.animationList = [];
+		const buttonIdSuffix: string = '_DisplayBtn';
 
 		this.animation.spineData.animations.forEach( animation => {
-			const buttonHTML: string = '<button id = ' + animation.name + '>' + animation.name + '</button><br>';
+			const buttonId: string = animation.name + buttonIdSuffix;
+			const buttonHTML: string = this.getButtonHtml( buttonId, animation.name );
 			buttonContainer.innerHTML += buttonHTML;
-			animationList.push( animation.name );;
+			this.animationList.push( animation.name );
 		} );
 
-		animationList.forEach( e => {
-			document.getElementById( e ).style.fontSize = '30px';
-			document.getElementById( e ).onclick = () => {
-				window.postMessage( { animationName: e }, '*' );
+		this.animationList.forEach( e => {
+			document.getElementById( e + buttonIdSuffix ).style.fontSize = `${ config.fontSize }px`;
+			document.getElementById( e + buttonIdSuffix ).onclick = () => {
+				if ( this.waitInputData.isWaiting ) {
+					window.postMessage( {
+						type: EventType.SET_ANIMATION_MIX,
+						data: {
+							animationName: e
+						}
+					}, '*' );
+				} else {
+					window.postMessage( {
+						type: EventType.PLAY_ANIMATION,
+						data: {
+							animationName: e
+						}
+					}, '*' );
+				}
 			};
 		} );
+	}
 
+	protected createAnimationMixer (): void {
+		const config = this.spineConfig.animationMixer;
+		this.animationMixer = document.getElementById( 'animationMixer' );
+		this.animationMixer.style.position = config.position;
+		this.animationMixer.style.overflow = config.overflow;
+		this.animationMixer.style.top = `${ config.y }px`;
+		this.animationMixer.style.left = `${ config.x }px`;
+		this.animationMixer.style.width = `${ config.width }px`;
+		this.animationMixer.style.height = `${ config.height }px`;
+
+		this.inputButtons = [];
+		this.createMixGroup();
+	}
+
+	protected createMixGroup (): void {
+		const config = this.spineConfig.animationMixer;
+		const btnPrefix: string = 'Get_';
+		const labelPrefix: string = 'GetLabel_';
+
+		const buttonId: string = btnPrefix + this.inputButtons.length.toString();
+		const labelId: string = labelPrefix + this.inputButtons.length.toString();
+		const inputButtonHTML: string = this.getInputFormHtml( buttonId, labelId );
+		this.animationMixer.innerHTML += inputButtonHTML;
+
+		const button = document.getElementById( buttonId );
+		const label = document.getElementById( labelId );
+
+		label.innerHTML = 'Empty';
+		label.style.fontSize = `${ config.fontSize }px`;
+		button.style.fontSize = `${ config.fontSize }px`;
+
+		button.onclick = () => {
+			if ( !this.waitInputData.isWaiting ) {
+				label.innerHTML = 'Waiting...';
+				this.waitInputData.isWaiting = true;
+				this.waitInputData.targetId = labelId;
+			}
+		};
+		this.inputButtons.push( button );
+		console.error( this.inputButtons[ 0 ].onclick );
+	}
+
+	protected addEventListener (): void {
 		window.addEventListener( "message", ( event ) => {
-			this.animation.renderable = true;
-			this.animation.state.setAnimation( 0, event.data.animationName, false );
+			const eventData: IPostMessage = event.data;
+			if ( eventData ) {
+				if ( eventData.type == EventType.PLAY_ANIMATION ) {
+					this.animation.renderable = true;
+					this.animation.state.setAnimation( 0, eventData.data.animationName, false );
+				} else if ( eventData.type == EventType.SET_ANIMATION_MIX ) {
+					document.getElementById( this.waitInputData.targetId ).innerHTML = eventData.data.animationName;
+					this.waitInputData.isWaiting = false;
+					this.createMixGroup();
+				}
+			}
 		}, false );
 	}
 
+	protected getButtonHtml ( id: string, text: string ): string {
+		return `<button id = "${ id }">${ text }</button><br>`;
+	}
+
+	protected getInputFormHtml ( buttonId: string, labelId: string ): string {
+		return `<input id = "${ buttonId }" type="submit" value="Get"><label id="${ labelId }"></label><br>`;
+	}
+
+}
+
+export enum EventType {
+	PLAY_ANIMATION = 'playAnimation',
+	SET_ANIMATION_MIX = 'setAnimationMix'
+}
+
+export interface IWaitInputData {
+	isWaiting: boolean;
+	targetId: string;
+}
+
+export interface IPostMessage {
+	type: string;
+	data: any;
 }
